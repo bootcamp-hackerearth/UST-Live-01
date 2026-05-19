13f269971aa8172dff14bbec84a744c621b259e6
Adithya Sreekumar <310507@ust.com>
2026-05-19 21:35:06 +0530

    Created possible exceptions

---
+++ HealthApp/HealthApp/Exceptions/PatientNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthApp.Exceptions
+{
+    public class PatientNotFoundException : Exception
+    {
+        public PatientNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ HealthApp/HealthApp/Exceptions/SlotAlreadyOverException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthApp.Exceptions
+{
+    public class SlotAlreadyOverException : Exception
+    {
+        public SlotAlreadyOverException(string message) : base(message)
+        {
+
+        }
+    }
+}
