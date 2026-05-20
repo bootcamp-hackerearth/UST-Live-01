a9ba09e6b087d8eca2e32feacb874712e80fab09
Adithya Sreekumar <310507@ust.com>
2026-05-20 12:48:51 +0530

    Added PatientRepository Unit Testing and updated Program.cs

--- HealthApp/HealthApp/Program.cs
+++ HealthApp/HealthApp/Program.cs
@@ -1,9 +1,14 @@
-﻿using HealthApp.Databases;
+﻿using HealthApp.Constants;
+using HealthApp.Databases;
+using HealthApp.Exceptions;
+using HealthApp.Models;
 using HealthApp.Repository.Impl;
 using HealthApp.Repository.Interface;
 using HealthApp.Service.Impl;
 using HealthApp.Service.Interface;
 using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 var services = new ServiceCollection();
 
@@ -29,4 +34,236 @@ var doctorService = provider.GetRequiredService<IDoctorService>();
 var appointmentService = provider.GetRequiredService<IAppointmentService>();
 var healthRecordService = provider.GetRequiredService<IHealthRecordService>();
 
-Console.WriteLine("Hello world");
+Console.Clear();
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("==========================================");
+    Console.WriteLine("        HEALTHCARE MANAGEMENT SYSTEM       ");
+    Console.WriteLine("==========================================\n");
+
+    Console.WriteLine("  [1] Register Patient");
+    Console.WriteLine("  [2] View All Patients");
+    Console.WriteLine("  [3] Update Patient Details");
+    Console.WriteLine("  [4] Add Doctor");
+    Console.WriteLine("  [5] Search Doctors");
+    Console.WriteLine("  [6] Exit\n");
+
+    Console.WriteLine("==========================================");
+    Console.Write("Select an option: ");
+
+
+    string? choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Patient patient = new Patient();
+
+            Console.Write("Name: ");
+            patient.FullName =
+                Console.ReadLine()!;
+
+            Console.Write("DOB (dd-mm-yyyy): ");
+            patient.DateOfBirth =
+                DateTime.Parse(
+                    Console.ReadLine()!);
+
+            Console.Write("Gender (Male/Female/Other): ");
+
+            string genderInput = Console.ReadLine()!;
+
+            //if (Enum.TryParse(genderInput, true, out GenderType gender))
+            //{
+            //    patient.Gender = gender;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Gender");
+            //    break;
+            //}
+
+            Console.Write("Phone: ");
+            patient.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Email: ");
+            patient.Email = Console.ReadLine()!;
+
+            Console.Write("Insurance ID: ");
+            patient.InsuranceId =
+                Console.ReadLine()!;
+
+            patientService.AddPatient(patient);
+
+            break;
+
+        case "2":
+
+            var patients = patientService.GetAll();
+
+            if (!patients.Any())
+            {
+                Console.WriteLine("No Patients Found");
+            }
+            else
+            {
+                foreach (var p in patients)
+                {
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine($"ID: {p.PatientId}");
+                    Console.WriteLine($"Name: {p.FullName}");
+                    Console.WriteLine($"DOB: {p.DateOfBirth.ToShortDateString()}");
+                    Console.WriteLine($"Gender: {p.Gender}");
+                    Console.WriteLine($"Phone: {p.PhoneNumber}");
+                    Console.WriteLine($"Email: {p.Email}");
+                    Console.WriteLine($"Insurance ID: {p.InsuranceId}");
+                }
+            }
+
+            break;
+        case "3":
+
+            try
+            {
+                Console.Write("Enter Patient ID to Update: ");
+
+                int updateId = int.Parse(Console.ReadLine()!);
+
+                Patient updatedPatient = new Patient();
+
+                Console.Write("Name: ");
+                updatedPatient.FullName = Console.ReadLine()!;
+
+                Console.Write("DOB (dd-mm-yyyy): ");
+                updatedPatient.DateOfBirth =
+                    DateTime.Parse(Console.ReadLine()!);
+
+                Console.Write("Gender (Male/Female/Other): ");
+
+                updatedPatient.Gender = Console.ReadLine()!;
+
+                //if (Enum.TryParse(genderInput, true,
+                //    out GenderType gender))
+                //{
+                //    updatedPatient.Gender = gender;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Invalid Gender");
+                //    break;
+                //}
+
+                Console.Write("Phone: ");
+                updatedPatient.PhoneNumber =
+                    Convert.ToInt64(Console.ReadLine())!;
+
+                Console.Write("Email: ");
+                updatedPatient.Email =
+                    Console.ReadLine()!;
+
+                Console.Write("Insurance ID: ");
+                updatedPatient.InsuranceId =
+                    Console.ReadLine()!;
+
+                string result =
+                    patientService.UpdatePatient(
+                        updateId,
+                        updatedPatient);
+
+                Console.WriteLine(result);
+            }
+            catch (PatientNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            break;
+
+        //case "4":
+
+        //    try
+        //    {
+        //        Console.Write("Enter Patient ID to Delete: ");
+
+        //        int deleteId =
+        //            int.Parse(Console.ReadLine()!);
+
+        //        string result =
+        //            patientService.DeletePatient(deleteId);
+
+        //        Console.WriteLine(result);
+        //    }
+        //    catch (PatientNotFoundException ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+
+        //    break;
+
+        case "4":
+
+            Doctor doctor = new();
+
+            Console.Write("Name: ");
+            doctor.FullName =
+                Console.ReadLine()!;
+
+            Console.Write("Specialisation: ");
+            doctor.Specialisation =
+                Console.ReadLine()!;
+
+            Console.Write("Experience: ");
+            doctor.YearsOfExperience =
+                int.Parse(
+                    Console.ReadLine()!);
+
+            //Console.Write("Email: ");
+            //doctor.DoctorEmail = Console.ReadLine()!;
+
+            //Console.Write("Phone Number: ");
+            //doctor.DoctorPhoneNo = Console.ReadLine()!;
+
+            Console.Write("Fee: ");
+            doctor.ConsultationFee =
+                (int)decimal.Parse(
+                    Console.ReadLine()!);
+
+
+            doctorService.AddDoctor(doctor);
+
+            //doctor.GetDoctorDetailsSummary();
+
+
+            break;
+
+        case "5":
+
+            Console.Write(
+                "Specialisation: ");
+
+            string specialisation =
+                Console.ReadLine()!;
+
+            var doctors =
+                doctorService
+                .GetDoctorsBySpecialisation(
+                    specialisation);
+
+            foreach (var d in doctors)
+            {
+                Console.WriteLine(
+                    $"ID:{d.DoctorId} " +
+                    $"Dr.{d.FullName}");
+            }
+
+            break;
+
+        case "6":
+            return;
+    }
+
+    Console.WriteLine(
+        "\nPress any key...");
+    Console.ReadKey();
+}
---
+++ HealthApp/HealthAppTests/PatientRepositoryTests.cs
@@ -0,0 +1,154 @@
+﻿using HealthApp.Databases;
+using HealthApp.Exceptions;
+using HealthApp.Models;
+using HealthApp.Repository.Impl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthAppTests
+{
+    public class PatientRepositoryTests
+    {
+        private readonly PatientDb _patientDb;
+        private readonly PatientRepository _patientRepository;
+
+        public PatientRepositoryTests()
+        {
+            _patientDb = new PatientDb();
+            //_patientDb.patient.AddRange(new List<Patient>
+            //{
+            //    new Patient                
+            //    {
+            //        PatientId = 1,
+            //        FullName = "John Doe",
+            //        DateOfBirth = new DateTime(1995, 5, 10),
+            //        Gender = "Male",
+            //        PhoneNumber = 9876543210,
+            //        Email = "john@example.com",
+            //        InsuranceId = "INS101"                
+            //    },
+            //    new Patient                
+            //    {
+            //        PatientId = 2,
+            //        FullName = "Jane Smith",
+            //        DateOfBirth = new DateTime(1998, 8, 15),
+            //        Gender = "Female",
+            //        PhoneNumber = 9123456780,
+            //        Email = "jane@example.com",
+            //        InsuranceId = "INS102"                
+            //    },
+            //    new Patient                
+            //    {
+            //        PatientId = 3,
+            //        FullName = "Robert Brown",
+            //        DateOfBirth = new DateTime(1985, 2, 20),
+            //        Gender = "Male",
+            //        PhoneNumber = 9988776655,
+            //        Email = "robert@example.com",
+            //        InsuranceId = "INS103"                
+            //    }
+            //});
+            _patientRepository = new PatientRepository(_patientDb);
+        }
+
+
+        [Fact]
+
+        public void GetAll_WhenCalled_ShouldReturn4Patients()
+        {
+            var patients = _patientRepository.GetAll();
+            Assert.NotNull(patients);
+            Assert.Equal(4, patients.Count);
+            Assert.Equal("Mark", patients[0].FullName);
+        }
+
+        [Fact]
+        public void GetById_GivenId_ReturnPatientWithSameId()
+        {
+            var patient = _patientRepository.GetById(2);
+            Assert.NotNull(patient);
+            Assert.Equal("Jane Smith", patient.FullName);
+            Assert.Equal("Female", patient.Gender);
+        }
+
+        [Fact]
+        public void Add_GivenPatientData_AddToPatientDb()
+        {
+            Patient patient = new Patient
+            {
+                PatientId = 4,
+                FullName = "Chris Evans",
+                DateOfBirth = new DateTime(1990, 12, 25),
+                Gender = "Male",
+                PhoneNumber = 9001122334,
+                Email = "chris@example.com",
+                InsuranceId = "INS104"
+            };
+
+            _patientRepository.AddPatient(patient);
+            var patients = _patientRepository.GetAll();
+            Assert.Equal(5, patients.Count);
+            Assert.Equal("Chris Evans", patients[3].FullName);
+        }
+
+        [Fact]
+        public void Update_GivenPatientIdAndPatient_UpdateThePatient()
+
+        {
+
+            Patient updatedPatient = new Patient
+            {
+                FullName = "Jane Williams",
+                DateOfBirth = new DateTime(1997, 7, 7),
+                Gender = "Female",
+                PhoneNumber = 9556677889,
+                Email = "jane.williams@example.com",
+                InsuranceId = "INS202"
+            };
+
+            var result = _patientRepository.UpdatePatient(2, updatedPatient);
+            Assert.NotNull(result);
+            Assert.Equal("Product with id 2 updated successfully", result);
+            var patient = _patientRepository.GetById(2);
+            Assert.Equal("Jane Williams", patient.FullName);
+            Assert.Equal("INS202", patient.InsuranceId);
+        }
+
+
+        [Fact]
+        public void Update_WhenProvidedWithInvalidId_ThrowsPatientNotFoundException()
+        {
+            Patient patient = new Patient
+            {
+                FullName = "Invalid Patient",
+                DateOfBirth = new DateTime(2000, 1, 1),
+                Gender = "Male",
+                PhoneNumber = 9999999999,
+                Email = "invalid@example.com",
+                InsuranceId = "INS999"
+            };
+            Assert.Throws<PatientNotFoundException>(() => _patientRepository.UpdatePatient(10, patient));
+        }
+
+        [Fact]
+        public void Delete_WhenProvidedWithPatientId_DeletePatient()
+        {
+            var result = _patientRepository.DeletePatient(3);
+            Assert.NotNull(result);
+            Assert.Equal("Product with id 3 deleted successfully", result);
+            Assert.Throws<PatientNotFoundException>(() => _patientRepository.GetById(3));
+        }
+        [Fact]
+        public void Delete_WhenProvidedWithInvalidId_ThrowsPatientNotFoundException()
+        {
+            Assert.Throws<PatientNotFoundException>(() => _patientRepository.DeletePatient(100));
+        }
+
+        [Fact]
+        public void GetById_WhenProvidedInvalidId_ThrowsPatientNotFoundException()
+        {
+            Assert.Throws<PatientNotFoundException>(() => _patientRepository.GetById(50));
+        }
+    }
+}
