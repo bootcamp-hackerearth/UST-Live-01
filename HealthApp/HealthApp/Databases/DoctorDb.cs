8c2e0a225219e2d3f9ae53789293eb732ba5b98f
Adithya Sreekumar <310507@ust.com>
2026-05-27 09:38:09 +0530

    updated doctor db

--- HealthApp/HealthApp/Databases/DoctorDb.cs
+++ HealthApp/HealthApp/Databases/DoctorDb.cs
@@ -12,9 +12,20 @@ namespace HealthApp.Databases
             new Doctor
             {
                 DoctorId = 1,
-                FullName = "Vimal Rajan",
+                FullName = "Dr. Arun",
+                Specialisation = SpecialisationType.GeneralPhysician,
+                DoctorPhoneNo = "9000000001",
+                DoctorEmail = "arun@gmail.com",
+                YearsOfExperience = 10,
+                ConsultationFee = 500m,
+                IsActive = true
+            },
+            new Doctor
+            {
+                DoctorId = 2,
+                FullName = "Dr. Vimal",
                 Specialisation = SpecialisationType.Cardiologist,
-                DoctorPhoneNo = "9876543210",
+                DoctorPhoneNo = "9000000002",
                 DoctorEmail = "vimal@gmail.com",
                 YearsOfExperience = 12,
                 ConsultationFee = 800m,
@@ -22,10 +33,10 @@ namespace HealthApp.Databases
             },
             new Doctor
             {
-                DoctorId = 2,
-                FullName = "Rahul Sharma",
+                DoctorId = 3,
+                FullName = "Dr. Rahul",
                 Specialisation = SpecialisationType.Dermatologist,
-                DoctorPhoneNo = "9123456780",
+                DoctorPhoneNo = "9000000003",
                 DoctorEmail = "rahul@gmail.com",
                 YearsOfExperience = 8,
                 ConsultationFee = 600m,
@@ -33,10 +44,21 @@ namespace HealthApp.Databases
             },
             new Doctor
             {
-                DoctorId = 3,
-                FullName = "Kumaravel Pandian",
+                DoctorId = 4,
+                FullName = "Dr. Meena",
+                Specialisation = SpecialisationType.Neurologist,
+                DoctorPhoneNo = "9000000004",
+                DoctorEmail = "meena@gmail.com",
+                YearsOfExperience = 14,
+                ConsultationFee = 1200m,
+                IsActive = true
+            },
+            new Doctor
+            {
+                DoctorId = 5,
+                FullName = "Dr. Kumar",
                 Specialisation = SpecialisationType.Orthopedic,
-                DoctorPhoneNo = "9988776655",
+                DoctorPhoneNo = "9000000005",
                 DoctorEmail = "kumar@gmail.com",
                 YearsOfExperience = 15,
                 ConsultationFee = 1000m,
@@ -44,13 +66,35 @@ namespace HealthApp.Databases
             },
             new Doctor
             {
-                DoctorId = 4,
-                FullName = "Muthu Krishnan",
+                DoctorId = 6,
+                FullName = "Dr. Lakshmi",
                 Specialisation = SpecialisationType.Pediatrician,
-                DoctorPhoneNo = "9012345678",
-                DoctorEmail = "muthu@gmail.com",
-                YearsOfExperience = 6,
-                ConsultationFee = 500m,
+                DoctorPhoneNo = "9000000006",
+                DoctorEmail = "lakshmi@gmail.com",
+                YearsOfExperience = 9,
+                ConsultationFee = 700m,
+                IsActive = true
+            },
+            new Doctor
+            {
+                DoctorId = 8,
+                FullName = "Dr. Suresh",
+                Specialisation = SpecialisationType.ENT,
+                DoctorPhoneNo = "9000000008",
+                DoctorEmail = "suresh@gmail.com",
+                YearsOfExperience = 7,
+                ConsultationFee = 650m,
+                IsActive = true
+            },
+            new Doctor
+            {
+                DoctorId = 9,
+                FullName = "Dr. Priya",
+                Specialisation = SpecialisationType.Gynecologist,
+                DoctorPhoneNo = "9000000009",
+                DoctorEmail = "priya@gmail.com",
+                YearsOfExperience = 13,
+                ConsultationFee = 950m,
                 IsActive = true
             }
         };
