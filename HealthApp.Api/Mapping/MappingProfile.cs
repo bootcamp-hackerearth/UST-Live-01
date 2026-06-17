fe364f2ae20a341a4b0426ff3dc2537c94666ff4
Adithya Sreekumar <310507@ust.com>
2026-06-18 01:53:04 +0530

    Updated Mapping profile

--- HealthApp.Api/Mapping/MappingProfile.cs
+++ HealthApp.Api/Mapping/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HealthApp.Api.Dtos;
+using HealthApp.Api.Enums;
 using HealthApp.Api.Models;
 
 namespace HealthApp.Api.Mapping
@@ -8,14 +9,61 @@ namespace HealthApp.Api.Mapping
     {
         public MappingProfile()
         {
-            CreateMap<Patient, PatientDto>().ReverseMap();
-            CreateMap<Doctor, DoctorDto>().ReverseMap();
-            CreateMap<Appointment, AppointmentDto>().ReverseMap();
-            CreateMap<HealthRecord, HealthRecordDto>().ReverseMap();
-            
+            // Patient mappings
+            CreateMap<Patient, PatientDto>();
 
+            CreateMap<PatientCreateDto, Patient>()
+                .ForMember(dest => dest.PatientId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTime.Now))
+                .ForMember(dest => dest.Appointments, opt => opt.Ignore())
+                .ForMember(dest => dest.HealthRecords, opt => opt.Ignore());
+
+            // Doctor mappings
+            CreateMap<Doctor, DoctorDto>()
+                .ForMember(dest => dest.Specialisation,
+                    opt => opt.MapFrom(src => src.Specialisation.ToString()));
+
+            CreateMap<DoctorCreateDto, Doctor>()
+                .ForMember(dest => dest.DoctorId, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+                .ForMember(dest => dest.Appointments, opt => opt.Ignore())
+                .ForMember(dest => dest.HealthRecords, opt => opt.Ignore());
+
+            // Appointment mappings
+            CreateMap<Appointment, AppointmentDto>()
+                .ForMember(dest => dest.PatientName,
+                    opt => opt.MapFrom(src => src.Patient != null ? src.Patient.FullName : string.Empty))
+                .ForMember(dest => dest.DoctorName,
+                    opt => opt.MapFrom(src => src.Doctor != null ? src.Doctor.FullName : string.Empty))
+                .ForMember(dest => dest.ScheduledDate,
+                    opt => opt.MapFrom(src => src.ScheduledDate.ToDateTime(TimeOnly.MinValue)))
+                .ForMember(dest => dest.Status,
+                    opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<AppointmentCreateDto, Appointment>()
+                .ForMember(dest => dest.AppointmentId, opt => opt.Ignore())
+                .ForMember(dest => dest.ScheduledDate,
+                    opt => opt.MapFrom(src => DateOnly.FromDateTime(src.ScheduledDate)))
+                .ForMember(dest => dest.Status,
+                    opt => opt.MapFrom(src => AppointmentStatus.Pending))
+                .ForMember(dest => dest.CancellationReason, opt => opt.Ignore())
+                .ForMember(dest => dest.Patient, opt => opt.Ignore())
+                .ForMember(dest => dest.Doctor, opt => opt.Ignore());
+
+            // HealthRecord mappings
+            CreateMap<HealthRecord, HealthRecordDto>()
+                .ForMember(dest => dest.PatientName,
+                    opt => opt.MapFrom(src => src.Patient != null ? src.Patient.FullName : string.Empty))
+                .ForMember(dest => dest.DoctorName,
+                    opt => opt.MapFrom(src => src.Doctor != null ? src.Doctor.FullName : string.Empty))
+                .ForMember(dest => dest.VisitDate,
+                    opt => opt.MapFrom(src => src.VisitDate.ToDateTime(TimeOnly.MinValue)));
+
+            CreateMap<HealthRecordCreateDto, HealthRecord>()
+                .ForMember(dest => dest.RecordId, opt => opt.Ignore())
+                .ForMember(dest => dest.Patient, opt => opt.Ignore())
+                .ForMember(dest => dest.Doctor, opt => opt.Ignore())
+                .ForMember(dest => dest.Appointment, opt => opt.Ignore());
         }
-
-
     }
 }
