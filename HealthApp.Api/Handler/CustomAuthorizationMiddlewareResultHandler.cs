3fda1bbfb749a6476db91ce6977fc44499ec31c9
Adithya Sreekumar <310507@ust.com>
2026-06-19 02:07:28 +0530

    Add custom authorization handler for auth failures

---
+++ HealthApp.Api/Handler/CustomAuthorizationMiddlewareResultHandler.cs
@@ -0,0 +1,48 @@
+﻿using HealthApp.Api.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace HealthApp.Api.Handler
+{
+    public class CustomAuthorizationMiddlewareResultHandler
+        : IAuthorizationMiddlewareResultHandler
+    {
+        private readonly AuthorizationMiddlewareResultHandler _defaultHandler = new();
+
+        public async Task HandleAsync(
+            RequestDelegate next,
+            HttpContext context,
+            AuthorizationPolicy policy,
+            PolicyAuthorizationResult authorizeResult)
+        {
+
+            if (authorizeResult.Challenged)
+            {
+                var hasAuthorizationHeader =
+                    context.Request.Headers.ContainsKey("Authorization");
+
+                if (!hasAuthorizationHeader)
+                {
+                    throw new UnauthorizedAccessAppException(
+                        "Please login to continue.");
+                }
+
+                throw new UnauthorizedAccessAppException(
+                    "Your session has expired. Please login again.");
+            }
+
+            if (authorizeResult.Forbidden)
+            {
+                throw new ForbiddenAccessException(
+                    "You do not have access to this page.");
+            }
+
+
+            await _defaultHandler.HandleAsync(
+                next,
+                context,
+                policy,
+                authorizeResult);
+        }
+    }
+}
