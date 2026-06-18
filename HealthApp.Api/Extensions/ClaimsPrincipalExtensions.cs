2b39a48aee8d6de5dac30a969ed1dc3f1e204caa
Adithya Sreekumar <310507@ust.com>
2026-06-19 02:04:09 +0530

    Created ClaimsPrincipalExtensions to access UserId, PatientId,DoctorId from the access token

---
+++ HealthApp.Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Claims;
+
+namespace HealthApp.Api.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static int? GetPatientId(this ClaimsPrincipal user)
+        {
+            var value = user.FindFirstValue("PatientId");
+
+            return int.TryParse(value, out var patientId)
+                ? patientId
+                : null;
+        }
+
+        public static int? GetDoctorId(this ClaimsPrincipal user)
+        {
+            var value = user.FindFirstValue("DoctorId");
+
+            return int.TryParse(value, out var doctorId)
+                ? doctorId
+                : null;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole("Admin");
+        }
+
+        public static bool IsPatient(this ClaimsPrincipal user)
+        {
+            return user.IsInRole("Patient");
+        }
+
+        public static bool IsDoctor(this ClaimsPrincipal user)
+        {
+            return user.IsInRole("Doctor");
+        }
+    }
+}
