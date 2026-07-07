3a1cd404ba11193fccc1f15f7733a94782b9e1dd
Aniket <287684@ust.com>
2026-07-07 09:56:19 +0530

    Background Services added

---
+++ HealthAxis API/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace HealthAxis.API.Models
+{
+    public class Notification
+    {
+        public int NotificationId { get; set; }
+
+        public int DoctorId { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
