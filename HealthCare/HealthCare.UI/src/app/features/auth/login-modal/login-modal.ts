159def77eafa90b3633dd59a6b179f0be8ea6568
Ajith-ust <310521@ust.com>
2026-07-12 13:09:58 +0530

    login fix

--- HealthCare/HealthCare.UI/src/app/features/auth/login-modal/login-modal.ts
+++ HealthCare/HealthCare.UI/src/app/features/auth/login-modal/login-modal.ts
@@ -14,7 +14,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoginModal {
 
-  @Output('modalClosed') modalClosed = new EventEmitter<void>();
+  @Output() modalClosed = new EventEmitter<void>();
 
   loginForm: FormGroup;
 
