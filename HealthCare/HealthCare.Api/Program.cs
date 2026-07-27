3636db7185b7e443941e38a5ffab1efd3ed025e7
Ajith-ust <310521@ust.com>
2026-07-27 10:58:49 +0530

    login redirection

--- HealthCare/HealthCare.Api/Program.cs
+++ HealthCare/HealthCare.Api/Program.cs
@@ -215,6 +215,13 @@ namespace HealthCare.Api
                 app.UseStaticFiles(new StaticFileOptions{ ContentTypeProvider = contentTypeProvider });
 
                 app.MapControllers();
+
+                app.MapGet("/", context =>
+                {
+                    context.Response.Redirect("/angular");
+
+                    return Task.CompletedTask;
+                });
                 app.MapGet("/angular", async context =>
                 {
                     await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, "angular","index.html"));
