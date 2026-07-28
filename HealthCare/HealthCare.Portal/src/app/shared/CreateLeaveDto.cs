b6a14dfcf0125c7ec935119fe9438db38b76aa7f
Suhana Ali <Suhana.Ali@ust.com>
2026-07-28 22:01:21 +0530

    Final Commit

---
+++ HealthCare/HealthCare.Portal/src/app/shared/CreateLeaveDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthCare.Api.DTOs.Doctor
+{
+    public class CreateLeaveDto
+    {
+
+        [Required]
+        public required DateOnly LeaveDate { get; set; }
+
+        [MaxLength(500)]
+        public string? Reason { get; set; }
+
+    }
+}
